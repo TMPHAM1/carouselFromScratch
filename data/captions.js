da7d235e28cd327117c5adb83ec127afe4b30df8
Tien Pham <tmpham1@uci.edu>
2019-01-03 16:39:03 -0800

    finished setting up env and adding functionality

---
+++ data/captions.js
@@ -0,0 +1,15 @@
+module.exports = [
+        {
+            "title": "First Dog Image",
+            "caption": "Basket of Puppies"
+        },
+        {
+            "title": "Second Dog Image",
+            "caption": "Bull Dog with a football"
+        },
+        {
+            "title": "Third Dog Image",
+            "caption": "Bull Dog"
+        }
+
+    ]
